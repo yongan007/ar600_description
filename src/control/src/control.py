1f5c774ce7b190c36be4a5023f0d10ec7ffdd7d0
yongan007 <yongan007@gmail.com>
2020-06-26 14:42:10 +0300

    init

--- src/control/src/control.py
+++ src/control/src/control.py
@@ -2,38 +2,51 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import Float64
-import math
+from numpy import pi 
 
 def talker():
     
     rospy.init_node('talker', anonymous=True)
 
-    pub3 = rospy.Publisher('/a600/joint3_position_controller/command', Float64, queue_size=10)
-    pub4 = rospy.Publisher('/Asimo_E1/joint4_position_controller/command', Float64, queue_size=10)
+    pub1 = rospy.Publisher('/a600/joint1_position_controller/command', Float64, queue_size=10)
+    pub2 = rospy.Publisher('/a600/joint2_position_controller/command', Float64, queue_size=10)
+    pub3 = rospy.Publisher('/a600/joint3_position_controller/command', Float64, queue_size=10)  
+    pub4 = rospy.Publisher('/a600/joint4_position_controller/command', Float64, queue_size=10)
+    pub5 = rospy.Publisher('/a600/joint5_position_controller/command', Float64, queue_size=10)
+    pub6 = rospy.Publisher('/a600/joint6_position_controller/command', Float64, queue_size=10)    
+    pub7 = rospy.Publisher('/a600/joint7_position_controller/command', Float64, queue_size=10)
+    pub8 = rospy.Publisher('/a600/joint8_position_controller/command', Float64, queue_size=10)
+    pub9 = rospy.Publisher('/a600/joint9_position_controller/command', Float64, queue_size=10)    
+    pub10 = rospy.Publisher('/a600/joint10_position_controller/command', Float64, queue_size=10)
+    pub11 = rospy.Publisher('/a600/joint11_position_controller/command', Float64, queue_size=10)
+    pub12 = rospy.Publisher('/a600/joint12_position_controller/command', Float64, queue_size=10)      
     
-    pub5 = rospy.Publisher('/Asimo_E1/joint5_position_controller/command', Float64, queue_size=10)
-    pub6 = rospy.Publisher('/Asimo_E1/joint6_position_controller/command', Float64, queue_size=10)
-    
-    pub7 = rospy.Publisher('/Asimo_E1/joint8_position_controller/command', Float64, queue_size=10)
-    pub8 = rospy.Publisher('/Asimo_E1/joint9_position_controller/command', Float64, queue_size=10)
-   
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-     
-        position = -math.pi/6+math.pi/12
-        position1 = math.pi/6
-        position2 = -math.pi/12
 
-        rospy.loginfo(position)
-        
-        pub3.publish(position)
-        pub4.publish(position)
+        position1 = pi/12
+        position2 = -pi/12
+        # position3 = -math.pi/12
+
         rate.sleep()
+
+        # rospy.loginfo(position)
+        
+        pub1.publish(position1)
+        pub2.publish(position1)
+        pub3.publish(position1)
+        pub4.publish(position1)
+        # rate.sleep()
         pub5.publish(position1)
         pub6.publish(position1)
 
         pub7.publish(position2)
         pub8.publish(position2)
+        pub9.publish(position2)
+        pub10.publish(position2)
+        pub11.publish(position2)
+        pub12.publish(position2)
+
         
         rate.sleep()
 
